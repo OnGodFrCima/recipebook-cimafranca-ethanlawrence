15a649c867d2fc8283fbaa555e16c0baceae0fde
OnGodFrCima <ethan.lawrence.cimafranca@student.ateneo.edu>
2026-02-09 23:22:23 +0800

    Fixed ledger URL and views error somehow

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -2,13 +2,12 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from recipebook.ledger import views
+from . import views
 
 urlpatterns = [
-    path('ledger/', include('ledger.urls', namespace="ledger")),
+    path('recipes/list/', views.recipe_list, name='recipe_list'),
     path('recipe/1/', views.recipe1, name='recipe1'),
     path('recipe/2/', views.recipe2, name='recipe2'),
-    path('admin/', admin.site.urls),
 ]
 # This might be needed, depending on your Django version
 app_name = "ledger"
