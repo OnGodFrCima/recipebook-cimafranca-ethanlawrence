2d641d7c586e3557f75ff5dfa3517482e7139971
OnGodFrCima <ethan.lawrence.cimafranca@student.ateneo.edu>
2026-02-19 21:43:04 +0800

    Finished adding admin panel

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -4,8 +4,10 @@ from .models import Recipe
 
 def recipe_list(request):
     recipes = Recipe.objects.all()
-    return render(request, 'ledger/recipe_list.html', {'recipes': recipes})
+    return render(request, 'recipes/recipe_list.html', {'recipes': recipes})
 
 def recipe_detail(request, pk):
     recipe = get_object_or_404(Recipe, pk=pk)
-    return render(request, 'ledger/recipe.html', {'recipe': recipe})
+    return render(request, 'recipes/recipe.html', {'recipe': recipe})
+
+
