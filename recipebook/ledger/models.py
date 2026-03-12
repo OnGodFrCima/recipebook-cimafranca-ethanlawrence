9eda496bcbf1cc5e4bb1447bc8069cac3bf001f4
OnGodFrCima <ethan.lawrence.cimafranca@student.ateneo.edu>
2026-03-12 19:27:30 +0800

    Fixed error with Migrations, had to recreate superuser

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
 
 def bio_length_error(value):
@@ -41,8 +41,8 @@ class Recipe(models.Model):
     
 class RecipeIngredient(models.Model):    
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name = "ingredients")
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name = "recipe")
-    quantity = models.CharField(max_length = 100)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name = "recipes")
+    quantity = models.CharField(max_length=100)
     
     def __str__(self):
         return f"{self.recipe.name}: {self.quantity} {self.ingredient.name}"
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,11 +1,14 @@
 # Create your views here.
 from django.shortcuts import get_object_or_404, render
 from .models import Recipe
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def recipe_list(request):
     recipes = Recipe.objects.all()
     return render(request, 'recipes/recipe_list.html', {'recipes': recipes})
 
+@login_required
 def recipe_detail(request, pk):
     recipe = get_object_or_404(Recipe, pk=pk)
     return render(request, 'recipes/recipe.html', {'recipe': recipe})
