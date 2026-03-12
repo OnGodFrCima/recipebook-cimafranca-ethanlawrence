a1033e7afae1c95f0bc087e35fdd347084d21bbc
OnGodFrCima <ethan.lawrence.cimafranca@student.ateneo.edu>
2026-03-12 18:58:17 +0800

    Finished Updating Profile MOdel and Reciple Model in models.

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 from django.urls import reverse
-
-
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 
+def bio_length_error(value):
+    if len(value) < 255:
+        raise ValidationError(
+            _("Bio length is too short. It must be at least 255 characters."),
+        )
+    
+        
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.TextField(validators=[bio_length_error])
+    
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
     
@@ -17,10 +29,12 @@ class Ingredient(models.Model):
 class Recipe(models.Model):
     
     name = models.CharField(max_length=100)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="recipes")
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     
     def __str__(self):
-        return self.name
-    
+        return self.name    
     def get_absolute_url(self):
         return reverse("ledger:recipe_detail", kwargs={"pk": self.pk})
     
